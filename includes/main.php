d667b2a3927d260aac5f154b136936bab6890855
Arnaud Barberon <barberon_a@hotmail.fr>
2022-02-04 12:22:25 +0100

    initial commit

---
+++ includes/main.php
@@ -0,0 +1,7 @@
+<?php
+require_once './includes/nav.php';
+?>
+<h1>GameLib</h1>
+<?php
+
+inclusionInc('home');
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require_once './functions/autoLoadFunction.php';
+session_start();
+date_default_timezone_set('Europe/Paris');
+// setlocale(LC_ALL, ''); spécifique Windows
+
+spl_autoload_register(function ($className) {
+    include './classes/' . $className . '.php';
+});
+
+require_once './includes/head.php';
+require_once './includes/main.php';
+require_once './includes/footer.php';
+
+$toto = new Sql;
