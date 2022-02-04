ff604b9ba239961786c1751c6054ac98e994bcd6
Arnaud Barberon <barberon_a@hotmail.fr>
2022-02-04 13:13:42 +0100

    maj 13:15

--- includes/frmInscription.php
+++ includes/frmInscription.php
@@ -1,11 +1,13 @@
-<form action="index.php?page=formulaire" method="post">
+<form action="index.php?page=inscription" method="post" enctype="multipart/form-data">
     <label for="nom">Nom :</label><input type="text" id="nom" name="nom" value="<?php echo $nom;?>" /><br />
     <label for="prenom">Prénom :</label><input type="text" id="prenom" name="prenom"  value="<?php echo $prenom;?>" /><br />
     <label for="email">e-mail :</label><input type="text" id="email" name="email"  value="<?php echo $email;?>" /><br />
+    <label for="pseudo">Pseudo :</label><input type="pseudo" id="pseudo" name="pseudo"  value="<?php echo $pseudo;?>" /><br />
     <label for="password">Mot de passe :</label><input type="password" id="password" name="password" /><br />
     <label for="passwordverif">Vérification mot de passe :</label><input type="password" id="passwordverif" name="passwordverif" /><br />
-    <label for="avatar"Avatar></label><input type="file" name="avatar" id="avatar" value="<?php echo $avatar;?>">
+    <label for="bio">Bio :</label><input type="bio" id="bio" name="bio"  value="<?php echo $bio;?>" /><br />
+    <label for="avatar">Avatar</label><input type="file" name="avatar" id="avatar" value="<?php echo $avatar;?>" /><br />
     <input type="reset" value="Effacer" />
-    <input type="submit" value="Clique-moi grand fou !" />
+    <input type="submit" value="S'inscrire" name="inscription" />
     <input type="hidden" name="frm" />
 </form>
